8b840d805702fcf39431d088ae2bfa06b4859ab9
Krugou <59641229+Krugou@users.noreply.github.com>
2023-08-31 12:40:09 +0300

    asd

--- tailwind-projekti/tailwind.config.js
+++ tailwind-projekti/tailwind.config.js
@@ -11,13 +11,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        aleksiblack: '#000000',
-        aleksigreen: '#2d996d',
-        aleksipurple: '#876484',
-        aleksidark: '#153640',
-        aleksicharcoal: '#0d1516',
+        aleblack: '#000000',
+        alegreen: '#2d996d',
+        alepurple: '#876484',
+        aledark: '#153640',
+        alecharcoal: '#0d1516',
       },
-},
+    },
   },
   plugins: [],
 }

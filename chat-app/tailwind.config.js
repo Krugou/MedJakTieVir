9a9480d64aaacb2eaafd3465b465b491903f43ee
Krugou <59641229+Krugou@users.noreply.github.com>
2023-09-06 13:07:07 +0300

    asd

---
+++ chat-app/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/**/*.html',
+    './public/**/*.js',
+  ],
+  corePlugins: {
+    // ...
+  },
+
+  theme: {
+    extend: {
+      backgroundImage: {
+        'aurora': "url('./img/aurora.png')",
+      },
+      colors: {
+        aleviolet: '#464660',
+        alegreen: '#2d996d',
+        alepurple: '#876484',
+        aledark: '#153640',
+        alecharcoal: '#0d1516',
+      },
+    },
+  },
+  plugins: [],
+};

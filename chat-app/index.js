2bdf02322ecf589aff77b3f48b829d5fa7e3be55
Krugou <59641229+Krugou@users.noreply.github.com>
2023-09-06 10:17:21 +0300

    asd

--- chat-app/index.js
+++ chat-app/index.js
@@ -6,23 +6,28 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const port = 3000;
-
+const lastTenMessages = [];
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
     console.log(socket.id, ' has entered the building');
-
+    console.log(lastTenMessages);
+    for (let i = 0; i < lastTenMessages.length; i++) {
+        socket.emit('chat message', lastTenMessages[i].username + ': ' + lastTenMessages[i].message);
+    }
     socket.on('disconnect', () => {
         console.log(socket.id, ' has left the building');
     });
-    // socket.on('set username', (username) => {
-    //     socket.username = username;
-    //     console.log(socket.id, ' has set username to ', username);
-    // });
+    ;
     socket.on('chat message', (data) => {
-    console.log(`message: ${data.message}, username: ${data.username}`);
-    io.emit('chat message', `${data.username}: ${data.message}`);
-  });
+        if (lastTenMessages.length > 10) {
+            lastTenMessages.shift();
+        }
+        lastTenMessages.push(data);
+
+        console.log(`message: ${data.message}, username: ${data.username}`);
+        io.emit('chat message', `${data.username}: ${data.message}`);
+    });
 });
 
 http.listen(port, () => {

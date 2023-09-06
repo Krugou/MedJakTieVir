a7112cc713fe0d304a9b815fcd1061118abe8c2d
Krugou <59641229+Krugou@users.noreply.github.com>
2023-09-06 12:48:01 +0300

    asd

--- chat-app/index.js
+++ chat-app/index.js
@@ -6,29 +6,52 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const port = 3000;
-const lastTenMessages = [];
+const lastTenMessages = {};
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
     console.log(socket.id, ' has entered the building');
-    console.log(lastTenMessages);
-    for (let i = 0; i < lastTenMessages.length; i++) {
-        socket.emit('chat message', lastTenMessages[i].username + ': ' + lastTenMessages[i].message);
-    }
+
     socket.on('disconnect', () => {
         console.log(socket.id, ' has left the building');
     });
-    ;
-    socket.on('chat message', (data) => {
-        if (lastTenMessages.length > 10) {
-            lastTenMessages.shift();
+    socket.on('join room', (room) => {
+        console.log(socket.id, ' joined room: ', room);
+        socket.join(room);
+        if (lastTenMessages) {
+            for (const room in lastTenMessages) {
+                if (lastTenMessages.hasOwnProperty(room)) {
+                    lastTenMessages[room].forEach((message) => {
+                        socket.emit('chat message', {message: message.message, username: message.username, room: message.room});
+                    });
+                }
+            }
         }
-        lastTenMessages.push(data);
-
-        console.log(`message: ${data.message}, username: ${data.username}`);
-        io.emit('chat message', `${data.username}: ${data.message}`);
+    });
+    socket.on('leave room', (room) => {
+        console.log(socket.id, ' left room: ', room);
+        socket.leave(room);
+    });
+    socket.on('chat message', (data) => {
+        if (!lastTenMessages[data.room]) {
+            lastTenMessages[data.room] = [];
+        }
+        if (lastTenMessages[data.room].length > 10) {
+            lastTenMessages[data.room].shift();
+        }
+        lastTenMessages[data.room].push(data);
+        console.log(`message: ${data.message}, username: ${data.username}, room: ${data.room}`);
+        io.to(data.room).emit('chat message', {message: data.message, username: data.username, room: data.room});
+    });
+    socket.on('get messages', (room) => {
+        if (lastTenMessages[room]) {
+            lastTenMessages[room].forEach((message) => {
+                socket.emit('chat message', {username: message.username, message: message.message});
+            });
+        }
     });
 });
+
 
 http.listen(port, () => {
     console.log('Are we happy yet? http://localhost:' + port + '/index.html up and running!');
--- chat-app/public/chat.js
+++ chat-app/public/chat.js
@@ -1,21 +1,45 @@
 'use strict';
 
 // Server URL below must point to your server, localhost works for local development/testing
-// const socket = io('http://localhost:3000');
-const socket = io('https://aleksinmasiina.northeurope.cloudapp.azure.com/');
-
+const socket = io('http://localhost:3000');
+// const socket = io('https://aleksinmasiina.northeurope.cloudapp.azure.com/');
+const roomSelect = document.getElementById('room');
 document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault();
+    const currentRoom = roomSelect.value;
     const inp = document.getElementById('m');
     const username = document.getElementById('username').value; // get the username from an input field
-    socket.emit('chat message', {message: inp.value, username: username}); // emit an object with the message and username
+    // get the room from a select field
+    socket.emit('chat message', {message: inp.value, username: username, room: currentRoom}); // emit an object with the message, username, and room
     inp.value = '';
 });
-
+roomSelect.addEventListener('change', (event) => {
+    const newRoom = event.target.value;
+    const currentRoom = document.getElementById('room').value; // get the room from a select field
+    document.getElementById('messages').innerHTML = '';
+    socket.emit('leave room', currentRoom);
+    socket.emit('join room', newRoom);
+    socket.emit('get messages', newRoom);
+});
 socket.on('chat message', (data) => {
-    const item = document.createElement('li');
-    // add tailwind classes to the list item
-    item.classList.add('bg-white', 'rounded', 'p-2', 'm-2', 'shadow', 'text-gray-800');
-    item.innerHTML = data;
-    document.getElementById('messages').appendChild(item);
+    console.log(data);
+    const currentRoom = roomSelect.value;
+    if (data.room === currentRoom) {
+        const item = document.createElement('li');
+        // add tailwind classes to the list item
+        item.classList.add('bg-white', 'rounded', 'p-2', 'm-2', 'shadow', 'text-gray-800');
+        item.innerHTML = `${data.username}: ${data.message}`;
+        document.getElementById('messages').appendChild(item);
+    }
+});
+// join the socket to a specific room
+socket.on('connect', () => {
+    const currentRoom = document.getElementById('room').value; // get the room from a select field
+    socket.emit('join room', currentRoom);
+});
+
+// leave the socket from a specific room
+socket.on('disconnect', () => {
+    const currentRoom = document.getElementById('room').value; // get the room from a select field
+    socket.emit('leave room', currentRoom);
 });

0d13dda17a2d6abe9a1e1db88b02bc0938d29639
Krugou <59641229+Krugou@users.noreply.github.com>
2023-09-06 10:10:31 +0300

    asd

--- chat-app/public/chat.js
+++ chat-app/public/chat.js
@@ -2,7 +2,7 @@
 
 // Server URL below must point to your server, localhost works for local development/testing
 //const socket = io('http://localhost:3000');
-const socket = io('https://aleksinmasiina.northeurope.cloudapp.azure.com/'); ';;
+const socket = io('https://aleksinmasiina.northeurope.cloudapp.azure.com/');
 
 document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault();

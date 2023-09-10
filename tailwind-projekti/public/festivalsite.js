6fa6642274bb652dade2ec73705e11407fae7c7c
Krugou <59641229+Krugou@users.noreply.github.com>
2023-09-10 16:29:20 +0300

    asd

--- tailwind-projekti/public/festivalsite.js
+++ tailwind-projekti/public/festivalsite.js
@@ -25,3 +25,37 @@ const playButtonFunction = () => {
     });
 };
 playButtonFunction();
+
+const about = document.querySelector('#about');
+const schedule = document.querySelector('#schedule');
+const tickets = document.querySelector('#tickets');
+
+
+// silly script to make text flash
+// Get all text elements
+const textElements = document.querySelectorAll('.mybasetext');
+
+// Set the timer to 5 minutes (in milliseconds)
+const timer = 5 * 60 * 1000;
+
+// Set the initial timeout to null
+let timeout = null;
+
+// Add event listener for mousemove
+document.addEventListener('mousemove', () => {
+    // Clear the timeout
+    clearTimeout(timeout);
+
+    // Remove the animate class from all text elements
+    textElements.forEach(element => {
+        element.classList.remove('animate');
+    });
+
+    // Set a new timeout
+    timeout = setTimeout(() => {
+        // Add the animate class to all text elements
+        textElements.forEach(element => {
+            element.classList.add('animate');
+        });
+    }, timer);
+});

554b95c5b62ee11074b13f7fca4d89de44c449de
Krugou <59641229+Krugou@users.noreply.github.com>
2023-09-10 14:50:56 +0300

    asd

--- tailwind-projekti/public/festivalsitelogic.js
+++ tailwind-projekti/public/festivalsitelogic.js
@@ -6,7 +6,33 @@
 const socket = io();
 const roomSelect = document.getElementById('room');
 let forgetUsernameClicked = false;
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
 
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('open');
+    nav.classList.toggle('open');
+});
+const playButtonFunction = () => {
+    const playButton = document.querySelector('#playButton');
+
+    playButton.addEventListener('click', () => {
+
+        const audio = document.querySelector('audio');
+        audio.addEventListener('ended', () => {
+            playButton.textContent = 'Play festival theme';
+            playButtonFunction();
+        });
+        audio.play();
+        playButton.textContent = 'Pause festival theme';
+        playButton.addEventListener('click', () => {
+            audio.pause();
+            playButton.textContent = 'Play festival theme';
+            playButtonFunction();
+        });
+    });
+};
+playButtonFunction();
 document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault();
     const currentRoom = roomSelect.value;

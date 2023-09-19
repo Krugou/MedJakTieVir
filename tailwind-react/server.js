083c034025c4d38019c91cc254df5873dbe53cb6
Krugou <59641229+Krugou@users.noreply.github.com>
2023-09-19 22:03:10 +0300

    asd

--- tailwind-react/server.js
+++ tailwind-react/server.js
@@ -1,10 +1,13 @@
 'use strict';
 /* simple chat app */
 
-const express = require('express');
+import express from 'express';
+import {createServer} from 'http';
+import {Server} from 'socket.io';
+
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = createServer(app);
+const io = new Server(http);
 const port = 3002;
 const lastMessages = {};
 
